f45cc9d428a761ba169417d2765c7659377a2074
Zakaria Farini <zakariafarini@192.168.1.3>
2023-08-01 19:09:21 +0100

    variable assignement

--- main.c
+++ main.c
@@ -8,16 +8,17 @@
 
 typedef enum {
     TOKEN_NUMBER = MAX_ASCII,
+    TOKEN_IDENTIFIER,
     TOKEN_UNKNOWN,
 } TokenType;
 
 typedef struct {
-    int type;
-    int value; 
-    int c0;
-    int c1;
+    int     type;
+    char    *name;
+    int     value; 
+    int     c0;
+    int     c1;
 } Token;
-
 
 char *ft_strchr(char *s, char c)
 {
@@ -53,7 +54,15 @@ Token *tokenize(char *s)
                 i++;
             }
         }
-        else if (ft_strchr("+-*/^()", s[i]))
+        else if (isalpha(s[i]) || s[i] == '_')
+        {
+            token->type = TOKEN_IDENTIFIER;
+            while (isalnum(s[i]) || s[i] == '_')
+                i++;
+            token->name = calloc(i - token->c0 + 1, 1);
+            memcpy(token->name, s + token->c0, i - token->c0);
+        }
+        else if (ft_strchr("+-*/^()=", s[i]))
         {
             token->type = s[i];
             i++;
@@ -71,11 +80,13 @@ typedef struct Node Node;
 
 enum NodeType {
     NODE_NUMBER,
+    NODE_VAR,
     NODE_BINOP,
 };
 
 struct Node {
     int     type;
+    Node    *next_expr;
     Node    *left;
     Node    *right;
     Token   *token;
@@ -114,7 +125,15 @@ Node *parse(Token *tokens)
     p.curr_token = 0;
     p.first_free_node = 0;
     
-    return parse_expr(&p, 1);
+    Node *res = parse_expr(&p, 1);
+    Node *curr = res;
+
+    while (p.tokens[p.curr_token].type)
+    {
+        curr->next_expr = parse_expr(&p, 0);
+        curr = curr->next_expr;
+    }
+    return res;
 }
 
 enum {
@@ -126,11 +145,12 @@ struct {
     int prec;
     int assoc;
 } op_table[MAX_ASCII] = {
-    ['+'] = {1, ASSOC_LEFT},
-    ['-'] = {1, ASSOC_LEFT},
-    ['*'] = {2, ASSOC_LEFT},
-    ['/'] = {2, ASSOC_LEFT},
-    ['^'] = {3, ASSOC_RIGHT},
+    ['='] = {1, ASSOC_RIGHT},
+    ['+'] = {2, ASSOC_LEFT},
+    ['-'] = {2, ASSOC_LEFT},
+    ['*'] = {3, ASSOC_LEFT},
+    ['/'] = {3, ASSOC_LEFT},
+    ['^'] = {4, ASSOC_RIGHT},
 };
 
 Token *get_curr_token(Parser *p)
@@ -152,33 +172,41 @@ Node *parse_atom(Parser *p)
     Node *res;
 
     Token *token = get_curr_token(p);
-    if (token->type != TOKEN_NUMBER && token->type != '(') {
-        printf("error: expected an atom at position %d\n", token->c0);
-        exit(1);
-    }
     if (token->type == TOKEN_NUMBER) {
         res = make_node(p, NODE_NUMBER);
         skip_token(p);
     }
+    else if (token->type == TOKEN_IDENTIFIER) {
+        res = make_node(p, NODE_VAR);
+        skip_token(p);
+    }
     else if (token->type == '(') {
         skip_token(p);
-        res = parse_expr(p, 1);
+        res = parse_expr(p, 0);
         if (get_curr_token(p)->type != ')')
             printf("error: expected ')' at position %d\n", get_curr_token(p)->c0);
     }
     else
-        assert(0);
+    {
+        printf("error: expected an atom at position %d\n", token->c0);
+        exit(1);
+    }
 
     return (res);
+}
+
+int is_bin_op(int type)
+{
+    return ft_strchr("+-*/^=", type) != 0;
 }
 
 Node *parse_expr(Parser *p, int min_prec)
 {
     Node *res = parse_atom(p);
-
+    
     while (1) {
         Token *token = get_curr_token(p);
-        if (token->type >= MAX_ASCII || op_table[token->type].prec < min_prec)
+        if (!is_bin_op(token->type) || op_table[token->type].prec < min_prec)
             break;
 
         int next_prec = op_table[token->type].prec;
@@ -242,12 +270,58 @@ int eval(Node *node)
 
 int curr_var_idx = 0;
 
+static struct {
+    char *name;
+    int idx;
+} vars_indices[128];
+
+int get_var_idx(char *name)
+{
+    int i = 0;
+
+    while (vars_indices[i].name && strcmp(vars_indices[i].name, name))
+        i++;
+    if (!vars_indices[i].name)
+    {
+      //  printf("variable '%s' used before declaration\n", name);
+        return (-1);
+    }
+    return vars_indices[i].idx;
+}
+
+void set_var_idx(char *name, int idx)
+{
+    int i = 0;
+
+    while (vars_indices[i].name && strcmp(vars_indices[i].name, name))
+        i++;
+    assert(i < 128);
+    vars_indices[i].name = name;
+    vars_indices[i].idx = idx;
+}
+
 void gen_ir(Node *node)
 {
     if (node->type == NODE_NUMBER)
     {
         node->var_idx = curr_var_idx++;
-        printf("x%d = %d\n", node->var_idx, node->value);
+        printf("x%d = %d\n", node->var_idx, node->token->value);
+    }
+    else if (node->type == NODE_VAR)
+    {
+        node->var_idx = get_var_idx(node->token->name);
+    }
+    else if (node->type == NODE_BINOP && node->token->type == '=')
+    {
+        gen_ir(node->right);
+
+        node->var_idx = get_var_idx(node->left->token->name);
+        if (node->var_idx < 0)
+        {
+            node->var_idx = curr_var_idx++;
+            set_var_idx(node->left->token->name, node->var_idx);
+        }
+        printf("x%d = x%d // %s = x%d\n", node->var_idx, node->right->var_idx, node->left->token->name, node->var_idx);
     }
     else if (node->type == NODE_BINOP)
     {
@@ -262,7 +336,7 @@ void gen_ir(Node *node)
 
 int main()
 {
-    char *s = "2 + 3 * 2^2 - 4 * (7 / 3)";
+    char *s = "a = 5\na = a * 2\n";
     Token *tokens = tokenize(s);
 
     for (int i = 0; tokens[i].type; i++)
@@ -271,14 +345,20 @@ int main()
             printf("Number(%d)", tokens[i].value);
         else if (tokens[i].type == TOKEN_UNKNOWN)
             printf("Unknown(%c)",   s[tokens[i].c0]);
+        else if (tokens[i].type == TOKEN_IDENTIFIER)
+            printf("Ident(%s)", tokens[i].name);
         else
             printf("('%c')", tokens[i].type);
         printf(" ");
     }
     printf("\n");
     Node *node = parse(tokens);
-    int res = eval(node);
-    printf("res = %d\n", res);
+ //   int res = eval(node);
+   // printf("res = %d\n", res);
 
-    gen_ir(node);
+    while (node)
+    {
+        gen_ir(node);
+        node = node->next_expr;
+    }
 }
