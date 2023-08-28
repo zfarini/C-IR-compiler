181c7117b2e9274ea80aa3ff36c7073cf2fb8f53
Zakaria Farini <zfarini@c3r7p4.1337.ma>
2023-08-28 21:57:27 +0100

    notes

--- old_code.c
+++ old_code.c
@@ -1,17 +1,16 @@
 
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
 
 
-
-#define M_PI_M_2  6.28318548202514648438
-#define M_PI  3.14159274101257324219
-#define M_PI_2  1.57079637050628662109
 
 int compare_float(double f1, double f2)
 {
  double precision = 0.00000000000000000001;
+
+ if (f1 < f2)
+ {
+	 assert 0;
+	 return -1;
+}
  if ((f1 - precision) < f2)
   {
  return -1;
@@ -26,19 +25,24 @@ int compare_float(double f1, double f2)
   }
 }
 
-float my_cos(float x)
+double cos(double x)
 {
-
+	double M_PI_M_2 = 6.28318548202514648438;
+	double M_PI = 3.14159274101257324219;
+	double M_PI_2 = 1.57079637050628662109;
 
  if( x < 0.0f ) 
   	x = -x;
-
- // if (0 <= compare_float(x,M_PI_M_2)) 
+  //if (0 <= compare_float(x,M_PI_M_2)) 
  	{
-  		//x = x - M_PI_M_2;
-		while (x > M_PI_M_2)
-		{
+  	//	x = x - M_PI_M_2;
+		while (x > M_PI_M_2)//0 <= compare_float(x, M_PI_M_2))
+	{
   			x = x - M_PI_M_2;
+		//	print(x, (char)'\n');
+		//	for (int i = 0; i < 1000000; i = i + 1)
+		//	{
+		//	}
 		}
   	}
 
@@ -50,21 +54,26 @@ float my_cos(float x)
  return 1.0f - (x*x/2.0f)*( 1.0f - (x*x/12.0f) * ( 1.0f - (x*x/30.0f) * (1.0f - (x*x/56.0f )*(1.0f - (x*x/90.0f)*(1.0f - (x*x/132.0f)*(1.0f - (x*x/182.0f)))))));
 }
 
-float my_sin(float x)
+float sin(float x)
 {
-	//float M_PI_2 = 1.57079637050628662109;
-	return my_cos(x - M_PI_2);
+	float M_PI_2 = 1.57079637050628662109;
+	return cos(x - M_PI_2);
 }
 
-void my_memset(char *ptr, char c, int size)
+void memset(char *ptr, char c, int size)
 {
 	for (int i = 0; i < size; i = i + 1)
 		ptr[i] = c;
 }
 
+
 // TODO: for (;;) + for (j = 0 ;;)
 int main()
 {
+	print(compare_float(1, 2), (char)'\n');
+	print(compare_float(1, 1), (char)'\n');
+	print(compare_float(1, 0), (char)'\n');
+	return 0;
 	char ascii[13];
 
 	ascii[0] = '.';
@@ -87,27 +96,27 @@ int main()
     char b[1760];
 	while (1)
 	{
-        my_memset(b,32,1760);
-        my_memset(z,0,7040);
+        memset(b,32,1760);
+        memset(z,0,7040);
 		j = 0;
         for(; 6.28>j; j=j+0.07) {
 			i = 0;
             for(; 6.28 >i; i=i+0.02) {
-                float my_sini=my_sin(i),
-                      my_cosj=my_cos(j),
-                      my_sinA=my_sin(A),
-                      my_sinj=my_sin(j),
-                      my_cosA=my_cos(A),
-                      my_cosj2=my_cosj+2,
-                      mess=1/(my_sini*my_cosj2*my_sinA+my_sinj*my_cosA+5),
-                      my_cosi=my_cos(i),
-                      my_cosB=my_cos(B),
-                      my_sinB=my_sin(B),
-                      t=my_sini*my_cosj2*my_cosA-my_sinj* my_sinA;
-                int x=40+30*mess*(my_cosi*my_cosj2*my_cosB-t*my_sinB),
-                    y= 12+15*mess*(my_cosi*my_cosj2*my_sinB +t*my_cosB),
+                float sini=sin(i),
+                      cosj=cos(j),
+                      sinA=sin(A),
+                      sinj=sin(j),
+                      cosA=cos(A),
+                      cosj2=cosj+2,
+                      mess=1/(sini*cosj2*sinA+sinj*cosA+5),
+                      cosi=cos(i),
+                      cosB=cos(B),
+                      sinB=sin(B),
+                      t=sini*cosj2*cosA-sinj* sinA;
+                int x=40+30*mess*(cosi*cosj2*cosB-t*sinB),
+                    y= 12+15*mess*(cosi*cosj2*sinB +t*cosB),
                     o=x+80*y,
-                    N=8*((my_sinj*my_sinA-my_sini*my_cosj*my_cosA)*my_cosB-my_sini*my_cosj*my_sinA-my_sinj*my_cosA-my_cosi *my_cosj*my_sinB);
+                    N=8*((sinj*sinA-sini*cosj*cosA)*cosB-sini*cosj*sinA-sinj*cosA-cosi *cosj*sinB);
                 if(22>y&&y>0&&x>0&&80>x&&mess>z[o]){
                     z[o]=mess;
 					int index = 0;
@@ -125,10 +134,9 @@ int main()
 			char c = 10;
 			if (k % 80)
 				c = b[k];
-			printf("%c", c);
+			print(c);
 		}
         A = A + 0.04;
         B = B + 0.02;
     }
 }
-
